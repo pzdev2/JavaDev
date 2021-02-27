8880489be5a0d14eb2f0af37ecca34ea2efda4ca
pzdev2 <pzdev2@gmail.com>
2021-02-27 17:30:14 +0100

    Print cars sort by key

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,26 @@ package com.company;
 
 import com.company.creatures.Human;
 import com.company.database.Connector;
+import com.company.devices.Car;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Connector.connect();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        Map<String, Car> cars = new HashMap<>();
 
-        Human.findAllHumans().forEach(System.out::println);
+        cars.put("Maluch", new Car(2010, "Fiat", "126p", "red", 10000.00));
+        cars.put("Rekin", new Car(2020, "BMW", "E21", "black", 100000.00));
+        cars.put("Jazz", new Car(2011, "Honda", "Jazz", "red", 20000.00));
+        cars.put("Ceed", new Car(2021, "Kia", "Ceed", "white", 50000.00));
+
+        cars.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Car>comparingByKey())
+                .forEach(System.out::println);
     }
 }

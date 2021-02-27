697a083cb61be4be5872f76eb9d1fb1ffd4e00c8
pzdev2 <pzdev2@gmail.com>
2021-02-27 17:00:10 +0100

    add connector to the database

--- src/com/company/database/Connector.java
+++ src/com/company/database/Connector.java
@@ -1,15 +1,17 @@
 package com.company.database;
 
-import com.company.Configuration;
-
 import java.sql.*;
 
 public class Connector {
 
+    private static final String USER = System.getenv("db_user");
+    private static final String PASSWORD = System.getenv("db_pass");
+    private static final String URL = System.getenv("db_url");
+
     private static Connection CONN;
 
     public static void connect() throws SQLException {
-        CONN = DriverManager.getConnection(Configuration.URL, Configuration.USER, Configuration.PASSWORD);
+        CONN = DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
     public static Statement getStatement() throws SQLException {
@@ -17,8 +19,7 @@ public class Connector {
     }
 
     public static void executeSql(String sql) throws SQLException {
-
-        CONN.createStatement().execute(sql);
+        getStatement().execute(sql);
     }
 
     public static ResultSet executeQuery(String sql) throws SQLException {

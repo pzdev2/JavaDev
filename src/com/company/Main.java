82dcf27d39d373eaa697068727f3beef0e5a669e
pzdev2 <pzdev2@gmail.com>
2021-02-27 15:11:47 +0100

    Print cars in alphabetical order of models

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,55 +1,20 @@
 package com.company;
 
-import com.company.creatures.Human;
-import com.company.creatures.Pet;
-import com.company.database.Connect;
-import com.company.database.Connector;
+import com.company.devices.Car;
 
-import java.sql.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Connect.open();
+        Map<String, Car> cars = new TreeMap<>();
 
-        var hamster = new Pet("Hamster", "Sara", 0.98);
-        var human = new Human("Human", "Jan", "Kowalski", 200.00);
+        cars.put("Maluch", new Car(2010, "Fiat", "126p", "red", 10000.00));
+        cars.put("Rekin", new Car(2020, "BMW", "E21", "black", 100000.00));
+        cars.put("Jazz", new Car(2011, "Honda", "Jazz", "red", 20000.00));
+        cars.put("Ceed", new Car(2021, "Kia", "Ceed", "white", 50000.00));
 
-//        try {
-//            hamster.save();
-//            human.save();
-//
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        }
-
-        String selAnimal = "select * from animal;";
-        String selHuman = "select * from human;";
-        try {
-            ResultSet rsAnimal = Connector.executeQuery(selAnimal);
-            ResultSet rsHuman = Connector.executeQuery(selHuman);
-
-
-            while (rsAnimal.next()) {
-                String species = rsAnimal.getString(2);
-                String name = rsAnimal.getString(3);
-                Double weight = rsAnimal.getDouble(4);
-                System.out.println("species: " + species + " name: " + name + " weight: " + weight);
-            }
-            System.out.println("==================");
-
-            while (rsHuman.next()) {
-                String firstName = rsHuman.getString("first_name");
-                String lastName = rsHuman.getString("last_name");
-                System.out.println("First name: " + firstName);
-                System.out.println("Last name: " + lastName);
-                System.out.println("-----------------------");
-
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        cars.forEach((k, v) -> System.out.println(k));
     }
 }
